bf58f8cc31b3df8a87fae43aaa91fb0c27f4dbac
MaximKorn <korneev.maxim97@yandex.ru>
2020-07-31 18:47:34 +0300

    Pages And Steps (working)

--- src/test/java/Pages/BasePage.java
+++ src/test/java/Pages/BasePage.java
@@ -7,9 +7,9 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class BasePage {
-    //public SelenideElement complianceButton = $x("//button[text()='Compliance']");
-    //public SelenideElement requestsButton = $x("//span/a[text()='Заявки']");
-    //public SelenideElement logoutButton = $x("//a[@title='Выход из приложения']");
+//    public SelenideElement complianceButton = $x("//button[text()='Compliance']");
+//    public SelenideElement requestsButton = $x("//span/a[text()='Заявки']");
+//    public SelenideElement logoutButton = $x("//a[@title='Выход из приложения']");
 
     public BasePage pressComplianceButton() {
         $x("//button[text()='Compliance']").click();
